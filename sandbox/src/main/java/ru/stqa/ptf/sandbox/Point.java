f7af4313fb1252918524af83e2ba5a844cfb4a7b
Daria <d.zaikina@eastbanctech.ru>
2019-04-09 20:30:57 +0700

    created method for calculate distance in class Point

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Point.java
@@ -10,4 +10,9 @@ public class Point {
         this.y = b;
     }
 
+    public double distance(Point p2) {
+        double dist = Math.sqrt((p2.x - this.x)*(p2.x - this.x) + (p2.y - this.y)*(p2.y - this.y));
+        return dist;
+    }
+
 }
