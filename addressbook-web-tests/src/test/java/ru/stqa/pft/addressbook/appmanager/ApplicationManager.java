82fba78afe6c5ab12eeb5a4cbc61b5b153c9e5d2
dariaZaikina <zaikinad@gmail.com>
2019-05-21 02:41:06 +0700

    Изменила сравнение браузеров

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -22,10 +22,9 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == BrowserType.FIREFOX) {
+        if(browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.CHROME) {
-          //  System.setProperty("webdriver.chrome.driver", "/usr/bin/chromdriver");
+        } else if (browser.equals(BrowserType.CHROME)){
             wd = new ChromeDriver();
         }
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
